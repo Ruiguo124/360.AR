41be25e094e6f5baf30787f712c8b2aa8c53f2b5
Wail Ayad <wailay@hotmail.fr>
2019-03-16 13:12:12 -0400

    ar scene + 3d model + dependecies

--- app/src/main/java/com/beta/a360ar/MainActivity.java
+++ app/src/main/java/com/beta/a360ar/MainActivity.java
@@ -1,15 +1,25 @@
 package com.beta.a360ar;
 
+import android.net.Uri;
 import android.os.Handler;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.google.ar.core.ArCoreApk;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.rendering.ModelRenderable;
 
 public class MainActivity extends AppCompatActivity {
 private Button mArButton;
+private Fragment arFragment;
+private ModelRenderable toyotaR;
+private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +27,39 @@ private Button mArButton;
         mArButton = findViewById(R.id.button);
         maybeEnableArButton();
 
+        ModelRenderable.builder()
+                // To load as an asset from the 'assets' folder ('src/main/assets/andy.sfb'):
+                .setSource(this, Uri.parse("Toyota Prius XW30 2010.sfb"))
+
+                // Instead, load as a resource from the 'res/raw' folder ('src/main/res/raw/andy.sfb'):
+                //.setSource(this, R.raw.andy)
+
+                .build()
+                .thenAccept(renderable -> toyotaR = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Log.e(TAG, "Unable to load Renderable.", throwable);
+                            return null;
+                        });
+        Node node = new Node();
+        node.setParent(arFragment.getArSceneView().getScene());
+        node.setRenderable(toyotaR);
+
+
+
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+
+    }
+
+
+
 
     void maybeEnableArButton() {
         ArCoreApk.Availability availability = ArCoreApk.getInstance().checkAvailability(this);
