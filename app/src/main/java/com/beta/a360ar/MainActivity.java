8ad90ad3277965e626d1b35a425442509c7f7a2f
Wail Ayad <wailay@hotmail.fr>
2019-03-16 10:47:59 -0400

    initial commit

---
+++ app/src/main/java/com/beta/a360ar/MainActivity.java
@@ -0,0 +1,45 @@
+package com.beta.a360ar;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.ar.core.ArCoreApk;
+
+public class MainActivity extends AppCompatActivity {
+private Button mArButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mArButton = findViewById(R.id.button);
+        maybeEnableArButton();
+
+
+    }
+
+    void maybeEnableArButton() {
+        ArCoreApk.Availability availability = ArCoreApk.getInstance().checkAvailability(this);
+        if (availability.isTransient()) {
+            // Re-query at 5Hz while compatibility is checked in the background.
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    maybeEnableArButton();
+                }
+            }, 200);
+        }
+        if (availability.isSupported()) {
+            mArButton.setVisibility(View.VISIBLE);
+            mArButton.setEnabled(true);
+            // indicator on the button.
+        } else { // Unsupported or unknown.
+            mArButton.setVisibility(View.INVISIBLE);
+            mArButton.setEnabled(false);
+        }
+    }
+
+
+}
